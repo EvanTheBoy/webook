09dcc3b389037a6e2ddb65fe4684a4b1aeba4a9b
Evan <evanparker945@gmail.com>
2024-01-19 13:45:02 +0800

    编写用户注册模块的业务代码

---
+++ internal/web/user.go
@@ -0,0 +1,10 @@
+package web
+
+import "github.com/gin-gonic/gin"
+
+type UserHandler struct {
+}
+
+func (u *UserHandler) RegisterUserRoutes(server *gin.Engine) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	server := gin.Default()
+
+	if err := server.Run(":8080"); err != nil {
+		return
+	}
 }
