9dd365be5bb6a0e7ef735bd1dfcd3578a600c14c
Evan <evanparker945@gmail.com>
2024-01-21 22:12:45 +0800

    明天准备重写一遍, 还需加强对项目的理解

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,7 +1,3 @@
 package domain
 
 // User 业务意义上的用户
-type User struct {
-	Email    string
-	Password string
-}
--- internal/repository/dao/user.go
+++ internal/repository/dao/user.go
@@ -1,20 +1,3 @@
 package dao
 
-import "context"
-
 // UserDAO 数据库, 存储意义上的用户
-type UserDAO struct {
-}
-
-func (dao *UserDAO) Insert(ctx context.Context, u User) error {
-
-}
-
-type User struct {
-	Id       uint
-	Email    string
-	Password string
-
-	CreatedTime int
-	UpdatedTime int
-}
