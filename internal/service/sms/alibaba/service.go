12935594e41d93a73f52a5a4ec7cf170e7e9e349
Evan <evanparker945@gmail.com>
2024-02-21 14:58:58 +0800

    短信发送技术的服务端代码编写完毕，准备测试

--- internal/service/sms/alibaba/service.go
+++ internal/service/sms/alibaba/service.go
@@ -3,70 +3,98 @@ package alibaba
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v3/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 	"github.com/ecodeclub/ekit"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 type Service struct {
-	signName *string
-	client   *dysmsapi20170525.Client
+	client *dysmsapi20170525.Client
 }
 
-func NewService(client *dysmsapi20170525.Client, signName string) *Service {
+func NewService(client *dysmsapi20170525.Client) *Service {
 	return &Service{
-		client:   client,
-		signName: ekit.ToPtr[string](signName),
+		client: client,
 	}
 }
 
-func (s *Service) Send(ctx context.Context, tpl string, args string, numbers string) error {
-	req := &dysmsapi20170525.SendSmsRequest{
-		PhoneNumbers:  ekit.ToPtr[string](numbers),
-		TemplateCode:  ekit.ToPtr[string](tpl),
-		TemplateParam: ekit.ToPtr[string](args),
-		SignName:      s.signName,
+func (s *Service) Send(ctx context.Context, tplId, signName string, numbers []string) error {
+	if len(numbers) == 0 {
+		return errors.New("电话号码为空")
 	}
-	runtime := &util.RuntimeOptions{}
-	tryErr := func() (e error) {
-		defer func() {
-			if r := tea.Recover(recover()); r != nil {
-				e = r
+	for i := 0; i < len(numbers); i++ {
+		phone := numbers[i]
+		code := GenValidateCode(6)
+		bcode, _ := json.Marshal(map[string]interface{}{
+			"code": code,
+		})
+		req := &dysmsapi20170525.SendSmsRequest{
+			PhoneNumbers:  ekit.ToPtr[string](phone),
+			TemplateCode:  ekit.ToPtr[string](tplId),
+			TemplateParam: ekit.ToPtr[string](string(bcode)),
+			SignName:      ekit.ToPtr[string](signName),
+		}
+		runtime := &util.RuntimeOptions{}
+		tryErr := func() (e error) {
+			defer func() {
+				if r := tea.Recover(recover()); r != nil {
+					e = r
+				}
+			}()
+			resp, err := s.client.SendSmsWithOptions(req, runtime)
+			if *resp.Body.Code == "OK" {
+				fmt.Println(phone, string(bcode))
 			}
+			if err != nil {
+				fmt.Println(errors.New(*resp.Body.Message))
+				return err
+			}
+			return nil
 		}()
-		_, err := s.client.SendSmsWithOptions(req, runtime)
-		if err != nil {
-			return err
-		}
-		return nil
-	}()
-	if tryErr != nil {
-		var e = &tea.SDKError{}
-		if _t, ok := tryErr.(*tea.SDKError); ok {
-			e = _t
-		} else {
-			e.Message = tea.String(tryErr.Error())
-		}
-		// 错误 message
-		fmt.Println(tea.StringValue(e.Message))
-		// 诊断地址
-		var data interface{}
-		d := json.NewDecoder(strings.NewReader(tea.StringValue(e.Data)))
-		err := d.Decode(&data)
-		if err != nil {
-			return err
-		}
-		if m, ok := data.(map[string]interface{}); ok {
-			recommend, _ := m["Recommend"]
-			fmt.Println(recommend)
-		}
-		_, err = util.AssertAsString(e.Message)
-		if err != nil {
-			return err
+		if tryErr != nil {
+			var e = &tea.SDKError{}
+			var t *tea.SDKError
+			if errors.As(tryErr, &t) {
+				e = t
+			}
+			// 错误 message
+			fmt.Println(tea.StringValue(e.Message))
+			// 诊断地址
+			var data interface{}
+			d := json.NewDecoder(strings.NewReader(tea.StringValue(e.Data)))
+			err := d.Decode(&data)
+			if err != nil {
+				return err
+			}
+			if m, ok := data.(map[string]interface{}); ok {
+				recommend, _ := m["Recommend"]
+				fmt.Println(recommend)
+			}
+			_, err = util.AssertAsString(e.Message)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func GenValidateCode(width int) string {
+	numeric := [10]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	r := len(numeric)
+	rand.NewSource(time.Now().UnixNano())
+	var sb strings.Builder
+	for i := 0; i < width; i++ {
+		_, err := fmt.Fprintf(&sb, "%d", numeric[rand.Intn(r)])
+		if err != nil {
+			return ""
+		}
+	}
+	return sb.String()
 }
