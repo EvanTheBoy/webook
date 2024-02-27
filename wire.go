62c74aa904ee7468c482e6eff31aa5266247276b
Evan <evanparker945@gmail.com>
2024-02-27 22:13:41 +0800

    继续重构项目代码

--- wire.go
+++ wire.go
@@ -1,9 +1,14 @@
+//go:build wireinject
+
 package main
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
+	"webook/internal/repository"
 	"webook/internal/repository/dao"
+	"webook/internal/service"
+	"webook/internal/web"
 	"webook/ioc"
 )
 
@@ -11,6 +16,9 @@ func InitWebServer() *gin.Engine {
 	wire.Build(
 		ioc.InitDB, initRedis,
 		dao.NewUserDao,
+		repository.NewUserRepository,
+		service.NewUserService,
+		web.NewUserHandler,
 	)
 	return new(gin.Engine)
 }
