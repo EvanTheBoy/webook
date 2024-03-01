254848b5e28ba5a9293f92a8e1e953d2e36c4cef
Evan <evanparker945@gmail.com>
2024-03-01 22:58:09 +0800

    使用wire帮助生成依赖式注入代码

--- wire.go
+++ wire.go
@@ -1,29 +1,46 @@
+//go:build wireinject
+
 package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
 	"webook/internal/repository"
 	"webook/internal/repository/cache"
 	"webook/internal/repository/dao"
 	"webook/internal/service"
-	"webook/internal/service/sms/memory"
 	"webook/internal/web"
 	"webook/ioc"
 )
 
 func InitWebServer() *gin.Engine {
-	cmdable := ioc.InitRedis()
-	v := ioc.InitMiddlewares(cmdable)
-	db := ioc.InitDB()
-	userDao := dao.NewUserDao(db)
-	userCache := cache.NewUserCache(cmdable)
-	userRepository := repository.NewUserRepository(userDao, userCache)
-	userService := service.NewUserService(userRepository)
-	codeCache := cache.NewCodeCache(cmdable)
-	codeRepository := repository.NewCodeRepository(codeCache)
-	smsService := memory.NewService()
-	codeService := service.NewCodeService(codeRepository, smsService)
-	userHandler := web.NewUserHandler(userService, codeService)
-	engine := ioc.InitGin(v, userHandler)
-	return engine
+	wire.Build(
+		ioc.InitDB, ioc.InitRedis,
+		dao.NewUserDao,
+		cache.NewUserCache,
+		cache.NewCodeCache,
+		repository.NewCodeRepository,
+		repository.NewUserRepository,
+		service.NewCodeService,
+		service.NewUserService,
+		ioc.InitSMSService,
+		web.NewUserHandler,
+		ioc.InitGin,
+		ioc.InitMiddlewares,
+	)
+	return new(gin.Engine)
+	//cmdable := ioc.InitRedis()
+	//v := ioc.InitMiddlewares(cmdable)
+	//db := ioc.InitDB()
+	//userDao := dao.NewUserDao(db)
+	//userCache := cache.NewUserCache(cmdable)
+	//userRepository := repository.NewUserRepository(userDao, userCache)
+	//userService := service.NewUserService(userRepository)
+	//codeCache := cache.NewCodeCache(cmdable)
+	//codeRepository := repository.NewCodeRepository(codeCache)
+	//smsService := memory.NewService()
+	//codeService := service.NewCodeService(codeRepository, smsService)
+	//userHandler := web.NewUserHandler(userService, codeService)
+	//engine := ioc.InitGin(v, userHandler)
+	//return engine
 }
