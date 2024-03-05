340ff139011c9bc4267878c374a3596c7f35c193
Evan <evanparker945@gmail.com>
2024-03-05 14:59:30 +0800

    开发服务容错治理的接口完毕

--- internal/service/sms/failover/service.go
+++ internal/service/sms/failover/service.go
@@ -2,6 +2,9 @@ package failover
 
 import (
 	"context"
+	"errors"
+	"log"
+	"sync/atomic"
 	"webook/internal/service/sms"
 )
 
@@ -10,10 +13,27 @@ type FailoverSMSService struct {
 	idx      uint64
 }
 
-func NewFailoverSMSService() sms.Service {
-
+func NewFailoverSMSService(services []sms.Service, idx uint64) sms.Service {
+	return &FailoverSMSService{
+		services: services,
+		idx:      idx,
+	}
 }
 
 func (f *FailoverSMSService) Send(ctx context.Context, tplId, signName, code string, numbers []string) error {
-
+	idx := atomic.AddUint64(&f.idx, 1)
+	length := uint64(len(f.services))
+	for i := idx; i < idx+length; i++ {
+		svc := f.services[int(i%length)]
+		err := svc.Send(ctx, tplId, signName, code, numbers)
+		switch {
+		case err == nil:
+			return nil
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return err
+		default:
+			log.Default()
+		}
+	}
+	return errors.New("全部服务商都失败了")
 }
