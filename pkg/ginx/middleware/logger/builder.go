fffaee0de6d98a255c6e0ba5c25643385f9a4fcf
Evan <evanparker945@gmail.com>
2024-03-10 15:08:12 +0800

    除去 ctx 响应之外的 http 请求字段都已拿到，正在想办法解决无法通过 ctx 拿到 http 响应字段的问题

--- pkg/ginx/middleware/logger/builder.go
+++ pkg/ginx/middleware/logger/builder.go
@@ -1,35 +1,66 @@
 package logger
 
 import (
+	"bytes"
+	"context"
 	"github.com/gin-gonic/gin"
+	"io"
 	"time"
 )
 
 type Builder struct {
-	allowReq  bool
-	allowResp bool
-	length    int
+	allowReqBody  bool
+	allowRespBody bool
+	length        int
+	loggerFunc    func(ctx context.Context, al *AccessLog)
 }
 
-func NewBuilder() *Builder {
+func NewBuilder(fn func(ctx context.Context, al *AccessLog)) *Builder {
 	return &Builder{
-		length: 1024,
+		length:     1024,
+		loggerFunc: fn,
 	}
 }
 
 func (b *Builder) AllowReq() *Builder {
-	b.allowReq = true
+	b.allowReqBody = true
 	return b
 }
 
 func (b *Builder) AllowResp() *Builder {
-	b.allowResp = true
+	b.allowRespBody = true
 	return b
 }
 
 func (b *Builder) Build() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		start := time.Now()
+		url := ctx.Request.URL.String()
+		if len(url) > b.length {
+			url = url[:b.length]
+		}
+		al := &AccessLog{
+			Method: ctx.Request.Method,
+			Url:    url,
+		}
+		if b.allowReqBody && ctx.Request.Body != nil {
+			body, _ := ctx.GetRawData()
+			ctx.Request.Body = io.NopCloser(bytes.NewReader(body))
+			if len(body) > b.length {
+				body = body[:b.length]
+			}
+			al.ReqBody = string(body)
+		}
+		defer func() {
+			al.Duration = time.Since(start)
+			if b.allowRespBody && ctx.Request.Body != nil {
+				// 打日志
+				b.loggerFunc(ctx, al)
 
+			}
+		}()
+		// 执行业务逻辑
+		ctx.Next()
 	}
 }
 
