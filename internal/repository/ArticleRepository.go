4cb0fcfc31ff34eca55a98a57c91a38b41b54eaf
Evan <evanparker945@gmail.com>
2024-03-12 16:12:30 +0800

    继续开发发帖功能

--- internal/repository/ArticleRepository.go
+++ internal/repository/ArticleRepository.go
@@ -2,9 +2,20 @@ package repository
 
 import (
 	"context"
-	"webook/internal/domain"
+	"webook/internal/repository/dao"
 )
 
 type ArticleRepository interface {
-	Edit(ctx context.Context, article domain.Article)
+	Edit(ctx context.Context, article dao.Article)
+}
+
+type ArticleRepositoryImpl struct {
+}
+
+func (a *ArticleRepositoryImpl) Edit(ctx context.Context, article dao.Article) {
+
+}
+
+func NewArticleRepository() ArticleRepository {
+	return &ArticleRepositoryImpl{}
 }
---
+++ internal/repository/dao/article.go
@@ -0,0 +1,6 @@
+package dao
+
+type Article struct {
+	Title   string
+	Content string
+}
